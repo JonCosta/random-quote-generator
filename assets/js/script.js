f997a9cb49381646f77125dc6650574c4b78b247
JonCosta <jonjcosta@gmail.com>
2017-05-25 23:06:57 -0300

    Added quotes file; changed page style back to default jumbotron; added function to get random quote

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
 $(function() {
 
+    // Generates when the the page is loaded
+    randomQuote();
+
+    // 
     $("#quoteBtn").click(function() {
-        console.log("Click!");
+        randomQuote();
     });
+
+    function randomQuote() {
+        $.getJSON("./assets/js/quotes.json", function(data) {
+            var id = Math.floor((Math.random() * data.quotes.length));
+            var quote = data.quotes[id];
+            $("#quote > h3").html(quote.phrase + " - "+ quote.author);
+        })
+    }
 
 });
