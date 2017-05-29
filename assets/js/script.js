5ccd5b99053ceac2df4e509bc132c3cc589d9b64
JonCosta <jonjcosta@gmail.com>
2017-05-29 16:31:54 -0300

    - Changed buttons position and style. - Added footer. - Added fade effect on clicking for new quote. - Tweet button opens a window of specific size. - Edited overall colors of the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    var currentQuote = "";
+
     $('[data-toggle="tooltip"]').tooltip();
 
     // Generates when the the page is loaded
@@ -7,17 +9,26 @@ $(function() {
 
     // Collects a new quote after pressing the button
     $("#quoteBtn").click(function() {
-        randomQuote();
+        $("#quote").fadeOut(500, function() {
+            randomQuote();
+            $("#quote").fadeIn(500);
+        });
+    });
+
+    $("#tweetBtn").click(function() {
+        window.open("https://twitter.com/intent/tweet?text="+ currentQuote.replace(" ", "%20"), "", "width=700,height=300");
     });
 
     function randomQuote() {
+        
         $.getJSON("./assets/js/quotes.json", function(data) {
             var id = Math.floor((Math.random() * data.quotes.length));
             var quote = data.quotes[id];
             $("#quote > h2").html(quote.phrase);
             $("#quote > h3").html("- "+ quote.author);
-            $(".my-twitter").attr("href", "https://twitter.com/intent/tweet?text="+ (quote.phrase+ ' - ' + quote.author).replace(' ', '%20'));
-        })
+            currentQuote = quote.phrase+ ' - ' + quote.author;
+            // $("#quote").fadeIn(1000);
+        });
     }
 
 });
